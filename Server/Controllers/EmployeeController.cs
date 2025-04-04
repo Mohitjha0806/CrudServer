0e85bbff98defecd32b8d904123b412531c6fd67
Mohit Jha <mohitjha95844@gmail.com>
2025-04-04 17:21:29 +0530

    Add project files.

---
+++ Server/Controllers/EmployeeController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly EmployeeContext _employeecontext; 
+        public EmployeeController(EmployeeContext employeeContext)
+        {
+            _employeecontext = employeeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+           if(_employeecontext.Employees == null)
+            {
+                return NotFound();
+            }
+            return await _employeecontext.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            if (_employeecontext.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _employeecontext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            
+            return employee;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if (_employeecontext.Employees == null)
+            {
+                return Problem("Entity set 'EmployeeContext.Employees' is null.");
+            }
+
+            employee.Id = 0;
+
+            _employeecontext.Employees.Add(employee);
+            await _employeecontext.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+            _employeecontext.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _employeecontext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+               
+                    throw;
+                
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            if (_employeecontext.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _employeecontext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _employeecontext.Employees.Remove(employee);
+            await _employeecontext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
+
---
+++ Server/Program.cs
@@ -0,0 +1,81 @@
+//using Microsoft.EntityFrameworkCore;
+//using Server.Models;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+//builder.Services.AddControllers();
+//// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+//builder.Services.AddCors();
+
+
+////builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    //app.MapOpenApi();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EmployeeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+
+builder.Services.AddCors();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors();
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:3000") // Replace with your frontend URL
+           .AllowAnyHeader()
+           .AllowAnyMethod());
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
